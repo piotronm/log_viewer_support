9d27092bda679671d9019a32bdb08d29caec6b13
piotronm <123025664+piotronm@users.noreply.github.com>
2023-10-10 13:10:49 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "./components/navigation/Navigation";
+import "./App.css";
+import SupportPortal from "./components/supportPortal/SupportPortal";
+
+function App() {
+  return (
+    <SupportPortal/>
+  );
+}
+
+export default App;
---
+++ components/supportPortal/SupportPortal.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import ToolHeader from "./toolHeader/ToolHeader";
+import TabComponent from "./tabComponent/TabComponent";
+
+const SupportPortal = () => {
+  return (
+    <>
+      {/* <div>
+        {" "}
+        <ToolHeader />{" "}
+      </div> */}
+      <div>
+        <TabComponent />{" "}
+      </div>
+    </>
+  );
+};
+
+export default SupportPortal;
