fc84ea94a9a8757c70529caab15718f23e69d436
piotronm <123025664+piotronm@users.noreply.github.com>
2023-10-16 15:43:08 -0400

    Update ApplicationTab.jsx

--- components/supportPortal/fetchData/ApplicationTab.jsx
+++ components/supportPortal/fetchData/ApplicationTab.jsx
@@ -28,11 +28,10 @@ function ApplicationsTab({ environment }) {
             jsonString +=
               "<div class='subsection'>" +
               jsonArray[i].replace(/\\r\\n/g, '').replace(/\\/g, '')
-                .replace(/"{/g, "}")
+                .replace(/"{/g, "{")
                 .replace(/}"/g, "}")
                 .replace(/{/g, "<div class='newsection'>{<div class='subsection'>")
-                .replace(/}/g, "</div>}</div>") +
-              "</div>";
+                .replace(/}/g, "</div>}</div>") + "</div>";
           }
           setApplicationsData(jsonString);
         } else {
